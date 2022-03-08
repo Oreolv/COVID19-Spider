c1d110259ccb29a48c709dd32388ae2c7a43348c
“Oreolv” <455001000@qq.com>
2022-03-08 15:07:58 +0800

    feat: 添加header

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
-import time, json, requests
+import json
+import time
+import requests
 
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
 # 腾讯疫情实时数据数据 URL
 url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=&_=%d' % int(
     time.time() * 1000)
-data = json.loads(requests.get(url=url).json()['data'])
-print(data)
+data = json.loads(requests.get(url=url, headers=headers).json()['data'])
