7c58c9800729d3d3e933df9dc60e95c8161992af
“Oreolv” <455001000@qq.com>
2022-03-08 16:07:03 +0800

    feat: areaTree数据分析

--- index.py
+++ index.py
@@ -10,3 +10,19 @@ headers = {
 url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=&_=%d' % int(
     time.time() * 1000)
 data = json.loads(requests.get(url=url, headers=headers).json()['data'])
+
+# 解析现有省市有数据
+all_data = []
+for item in data['areaTree'][0]['children']:
+    areaItem = {}
+    areaItem.update(name=item['name'],
+                    today=item['today'],
+                    total=item['total'],
+                    children=[])
+    for i in item['children']:
+        if (i['total']['nowConfirm']) > 0:
+            areaItem['children'].append(i)
+    all_data.append(areaItem)
+
+with open('areaTree.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(all_data, ensure_ascii=False))
