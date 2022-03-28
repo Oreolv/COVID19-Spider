8d72a7b20109ca43a888dc20a0404df26d8df77d
“Oreolv” <455001000@qq.com>
2022-03-28 22:14:22 +0800

    feat: 更改写入路径

--- index.py
+++ index.py
@@ -78,7 +78,7 @@ def get_city_data(province_temp_data):
 
 
 def write_json(dir, data):
-    with open('../Barrier-Server/static/{}.json'.format(dir),
+    with open('../Guard-Server/static/{}.json'.format(dir),
               'w',
               encoding='utf-8') as f:
         f.write(json.dumps(data, ensure_ascii=False))
