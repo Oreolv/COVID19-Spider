2ca12046b83737be9b84354de71cb65da5538bfe
“Oreolv” <455001000@qq.com>
2022-04-27 21:29:26 +0800

    feat: 停止爬取无用数据

--- index.py
+++ index.py
@@ -9,12 +9,12 @@ import covid
 if __name__ == '__main__':
     all_data = covid.get_all_data()
     risk_area = risk.get_risk_area()
-    risk_area_merge = risk.get_risk_area_merge()
+    # risk_area_merge = risk.get_risk_area_merge()
     news_list = news.get_news_list()
-    tips_list = tips.get_tips_list()
+    # tips_list = tips.get_tips_list()
 
     util.write_json('all_data', all_data)
     util.write_json('risk_area', risk_area)
-    util.write_json('risk_area_merge', risk_area_merge)
+    # util.write_json('risk_area_merge', risk_area_merge)
     util.write_json('news_list', news_list)
-    util.write_json('tips_list', tips_list)
+    # util.write_json('tips_list', tips_list)
